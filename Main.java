439cf484ce165500eac44e75b8e649aa78c40670
endricowisnu <128448826+endricowisnu@users.noreply.github.com>
2024-03-02 16:41:34 +0700

    Adding menu on main app

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int menu;
+
+        do{
+            System.out.println("=======Kalkulator=======");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.println("5. Keluar");
+            System.out.println("Silahkan input menu pilihan anda:");
+
+            menu = scanner.nextInt();
+            if(menu == 1){
+
+            }else if(menu == 2){
+
+            }else if(menu == 3){
+
+            }else if(menu == 4){
+
+            }else if(menu == 5){
+
+            }else{
+                System.out.println("Silahkan input angka yang ada di dalam menu");
+            }
+        }while(true);
     }
 }
